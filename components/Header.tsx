e955ab95705cc329e061d22db14f87daaa95d511
Indrajitn <indrajitmaji.20.cs@iite.indusuni.ac.in>
2026-08-30 02:04:04 +0530

    initial commit

---
+++ components/Header.tsx
@@ -0,0 +1,40 @@
+import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
+import { Zap } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className="w-full fixed top-0 left-0 z-50 h-16 border-b border-white/6 bg-white/7 backdrop:blur-md">
+      <nav className="mx-auto flex h-full max-w-7xl items-center justify-between px-4 sm:px-6">
+        <Link href="/">
+          <Image src={"/logo.png"} alt="Forge Logo" width={100} height={100} 
+          className="w-auto h-9 rounded-md"
+          />
+        </Link>
+        <div className="flex items-center gap-5">
+            <Show when="signed-in">
+              <Link href={"/projects"} className="text-[13px] font-medium text-white/40 transition-colors hover:text-white/80">Projects</Link>
+            <span className="inline-flex h-8 items-center gap-1.5 rounded-full border border-white/10 bg-white/5 px-3 text-xs text-white/70">
+              <Zap className="h-3 w-3 fill-white/70" />
+              3 / 40 credits
+            </span>
+            <UserButton />
+          </Show>
+
+          <Show when="signed-out">
+              <SignInButton />
+              <SignUpButton>
+                <button className="bg-purple-700 text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                  Sign Up
+                </button>
+              </SignUpButton>
+            </Show>
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
