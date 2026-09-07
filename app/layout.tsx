de685c38a1b0078ffe672670f84718182dbe7ebc
Indrajitn <indrajitmaji.20.cs@iite.indusuni.ac.in>
2026-09-08 03:29:53 +0530

    feat(auth): protect app routes and show user credit balance

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { dark } from "@clerk/themes";
 import { ThemeProvider } from "@/components/theme-provider";
 import Header from "@/components/Header";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "@/components/ui/sonner";
 
 const lora = Lora({
   subsets: ["latin"],
@@ -50,6 +51,7 @@ export default function RootLayout({
             <main>
               {children}
             </main>
+            <Toaster richColors/>
           </ThemeProvider>
         </body>
     </html>
